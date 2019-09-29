3fce19003fdf04bbc6b23a0ac82333e833d02e36
Bryan Smith <bryan.smith@logos.com>
2019-09-29 11:46:56 -0700

    Explicitly stringify output.

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const github = require('@actions/github');
 const minimatch = require('minimatch');
 
 function run() {
-	const pattern = core.getInput('patterns');
-	let patterns = core.getInput('patterns') || core.getInput('pattern');
-	if (!Array.isArray(patterns)) {
-		patterns = [patterns].filter(x => x);
-	}
+	const pattern = core.getInput('pattern');
 
-	const { added, removed, modified } = getMatch(github.context.payload, patterns);
+	const { added, removed, modified } = getMatch(github.context.payload, pattern);
 
-	core.setOutput('changed', added || removed || modified);
-	core.setOutput('added', added);
-	core.setOutput('removed', removed);
-	core.setOutput('modified', modified);
+	core.setOutput('changed', (added || removed || modified).toString());
+	core.setOutput('added', added.toString());
+	core.setOutput('removed', removed.toString());
+	core.setOutput('modified', modified.toString());
 }
 
-function getMatch(payload, patterns) {
+function getMatch(payload, pattern) {
 	let added = false;
 	let removed = false;
 	let modified = false;
 
-	const match = x => patterns.some(p => minimatch(x, p));
+	const match = x => minimatch(x, pattern);
 
 	for (const commit of payload.commits) {
 		added = added || commit.added.some(match);
